ae4ead53332ab0933f75c44845558777c38a1727
Pavel Lebedev <pavel.ps-89@yandex.ru>
2021-04-22 22:20:19 +0300

    popup finish

---
+++ script/script.js
@@ -0,0 +1,22 @@
+let blockPopup = document.querySelector('.popup');
+let closePopup = document.querySelector('.popup__close');
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_profession');
+
+function togglePopup(){
+blockPopup.classList.toggle('popup_open');
+nameInput.value = nameFromePage.textContent;
+jobInput.value = jobFromPage.textContent;
+}
+
+function closePopup(){
+    blockPopup.classList.remove("popup_open")
+}
+function savePopup(){
+    Event.preventDefault():
+    nameInput.textContent = nameInput.value;
+    jobInput.textContent = jobInput.value;
+}
+
+blockPopup.addEventListener('click', togglePopup);
+closePopup.addEventListener('click',togglePopup);
