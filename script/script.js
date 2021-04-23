696bfa74ef11f61d19cd337bc15cff5918bd8910
Pavel Lebedev <pavel.ps-89@yandex.ru>
2021-04-23 23:44:02 +0300

    123

--- script/script.js
+++ script/script.js
@@ -4,14 +4,14 @@ let closeBlockPopup = blockPopup.querySelector('.popup__close');
 let nameFromPage = document.querySelector('.profile__name');
 let jobFromPage = document.querySelector('.profile__profession');
 let nameFromForm = document.querySelector('.popup__input_name');
-let jobFromForm = document.querySelector('.popup__input_profession');
+let jobFromForm = document.querySelector('.popup__input_work');
 function togglePopup(){
-  blockPopup.classList.toggle('popup_opened');
+  blockPopup.classList.toggle('popup_open');
   nameFromForm.value = nameFromPage.textContent;
   jobFromForm.value = jobFromPage.textContent;
 }
 function closePopup(){
-  blockPopup.classList.remove('popup_opened');
+  blockPopup.classList.remove('popup_open');
 }
 function savePopup (event){
   event.preventDefault();
