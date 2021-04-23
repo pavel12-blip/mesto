ae15befa5a7eaeb9db44515a102f441304afc767
Pavel Lebedev <pavel.ps-89@yandex.ru>
2021-04-23 22:02:20 +0300

    123

--- script/script.js
+++ script/script.js
@@ -1,24 +1,24 @@
-let blockPopup = document.querySelector('.popup');
-let closePopup = document.querySelector('.popup__close');
-let nameInput = document.querySelector('.popup__input_name');
-let jobInput = document.querySelector('.popup__input_profession');
-
-function togglePopup(){
-blockPopup.classList.toggle('popup_open');
-nameInput.value = nameFromePage.textContent;
-jobInput.value = jobFromPage.textContent;
+let blockPopup = document.querySelector(".popup");
+let openBlockPopup = document.querySelector(".button__transition");
+let closeBlockPopup = blockPopup.querySelector(".popup__close");
+let nameFromPage = document.querySelector(".profile__name");
+let jobFromPage = document.querySelector(".profile__profession");
+let nameFromForm = document.querySelector(".popup__input_name");
+let jobFromForm = document.querySelector(".popup__input_profession");
+function togglePopup() {
+  blockPopup.classList.toggle("popup_opened");
+  nameFromForm.value = nameFromPage.textContent;
+  jobFromForm.value = jobFromPage.textContent;
 }
-
-function closePopup(){
-    blockPopup.classList.remove("popup_open")
+function closePopup() {
+  blockPopup.classList.remove("popup_opened");
 }
-function savePopup(){
-    Event.preventDefault();
-    nameInput.textContent = nameInput.value;
-    jobInput.textContent = jobInput.value;
-    closePopup()
+function savePopup(event) {
+  event.preventDefault();
+  nameFromPage.textContent = nameFromForm.value;
+  jobFromPage.textContent = jobFromForm.value;
+  closePopup();
 }
-
-blockPopup.addEventListener('submit', togglePopup);
-closeBlockPopup.addEventListener('click', togglePopup);
-closePopup.addEventListener('click',togglePopup);
+openBlockPopup.addEventListener("click", togglePopup);
+closeBlockPopup.addEventListener("click", togglePopup);
+blockPopup.addEventListener("submit", savePopup);
